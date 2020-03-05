62d24feb6356498690ee6f708421fbd1ed305991
Slava <slavkaklimov@gmail.com>
2020-03-05 18:40:07 +0200

    test compo

---
+++ lesson3/task3/src/UserInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Avatar from './Avatar';
+import './author.scss';
+
+const UserInfo = props => {
+    return (
+        <div className="user-info">
+            <Avatar avatarUrl={props.author.avatarUrl}/>
+            <div className="user-info__name">{props.author.name}</div>
+        </div>
+    );
+};
+
+export default UserInfo;
---
+++ lesson3/task3/src/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import "./index.scss";
+import Comment from "./Comment";
+
+
+const rootElem = document.querySelector('#root');
+const authorInfo = {
+    name: 'Tom',
+    avatarUrl: 'https://github.com/Slavon0007'
+};
+
+
+ReactDOM.render(
+    <Comment
+        author={authorInfo}
+        text="Good job!"
+        date={new Date("2020-02-06T22:38:20.566Z")}
+    />,
+     rootElem
+    );
