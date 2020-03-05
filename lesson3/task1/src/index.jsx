64ca6e13b70ab732d4dc31ec5198bbce9d20ea46
Slava <slavkaklimov@gmail.com>
2020-03-05 13:21:14 +0200

    compos

--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -5,7 +5,7 @@ import './search.scss';
 
 const rootElement = document.querySelector('#root');
 
-const Search = (props) => {
+export const Search = (props) => {
  return (
     <div className="search">
       <h1 className="search__title">{`Hello, ${props.name}. What to search for you`}</h1>
