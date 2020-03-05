06eeb60eb990be07640ba6028ea7c60752711cae
Slava <slavkaklimov@gmail.com>
2020-03-05 14:12:59 +0200

    fixed

--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './styles.css';
 import './search.scss';
 
-const rootElement = document.querySelector('#root');
+import Search from './Search.jsx';
 
-const SearchField = (props) => {
- return (
-    <div className="search">
-      <h1 className="search__title">{`Hello, ${props.name}. What to search for you`}</h1>
-      <div className="search__field">
-        <input type="text" className="search__input"/>
-        <button className="search__button">Search</button>
-      </div>
-    </div>
-  );
-};
+const rootElem = document.querySelector('#root');
 
-
-ReactDOM.render(<SearchField name="Tom" />, rootElement);
-
-export default SearchField;
+ReactDOM.render(<Search name="Tom" />, rootElem);
--- lesson3/task1/webpack.config.js
+++ lesson3/task1/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
-
+ 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === "production";
     const config = {
