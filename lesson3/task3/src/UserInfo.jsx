8c04274f5d7e90bcb667fd1f54cbdac8509eff16
Slava <slavkaklimov@gmail.com>
2020-03-06 17:48:50 +0200

    test

--- lesson3/task3/src/UserInfo.jsx
+++ lesson3/task3/src/UserInfo.jsx
@@ -5,8 +5,12 @@ import  './comment.scss';
 const UserInfo = props => {
     return (
         <div className="user-info">
-            <Avatar avatarUrl={props.author.avatarUrl}/>
-            <div className="user-info__name">{props.author.name}</div>
+            <Avatar avatarUrl={props.author.avatarUrl}
+            name={props.author.name}
+            />
+            <div className="user-info__name">
+                {props.author.name}
+            </div>
         </div>
     );
 };
