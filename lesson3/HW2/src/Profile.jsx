c29c730035553a0bd03d484d6b42b8c6a6beb823
Slava <slavkaklimov@gmail.com>
2020-03-10 14:18:08 +0200

    profile

--- lesson3/HW2/src/Profile.jsx
+++ lesson3/HW2/src/Profile.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import moment from "moment";
+import ReactDOM from 'react-dom';
+
+const rootElem = document.querySelector('#root');
+
+const userData = {
+    firstName: 'John',
+    lastName: 'Doe',
+    birthDate: '1991-01-17T11:11:11.819Z',
+    birthPlace: 'London'
+}
+
+ReactDOM.render(<Greeting userData={userData}/>, rootElem);
 
 const getDate = date => moment(date).format("DD MMM YY");
 
