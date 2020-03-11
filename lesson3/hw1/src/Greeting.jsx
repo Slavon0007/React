da527f491b0e5786edcde46d7a027797b675aa15
Slava <slavkaklimov@gmail.com>
2020-03-11 19:42:55 +0200

    aaaa

--- lesson3/hw1/src/Greeting.jsx
+++ lesson3/hw1/src/Greeting.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const age = Math.floor((new Date() - new Date(birthDate)) / new Date(3600*24*1000));
+const age = Math.floor((new Date() - new Date(birthDate)) / 1000*60*60*24*360);
 
 const Greeting = props => { 
     return (
         <div className="greeting">
             {`My name is
-             ${props.name}
+             ${props.firstName}
+             ${props.lastName}
              . I'm
              ${age(props.birthDate)} years old`}
         </div>
--- lesson3/hw1/src/index.jsx
+++ lesson3/hw1/src/index.jsx
@@ -5,6 +5,7 @@ import Greeting from './Greeting';
 const rootElement = document.querySelector('#root');
 
 ReactDOM.render(<Greeting
-    name = "John Doe"
+    firstName="John"
+    lastName="Doe"
     birthDate={"2003-11-06T22:00:00.000Z"}
 />, rootElement);
