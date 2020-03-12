3f49f055eb32cf750ac8d219b04a1e30e7902740
Slava <slavkaklimov@gmail.com>
2020-03-12 20:50:28 +0200

    greting2

--- lesson3/HW2/src/Profile.jsx
+++ lesson3/HW2/src/Profile.jsx
@@ -1,36 +1,23 @@
 import React from 'react';
 import moment from "moment";
-import ReactDOM from 'react-dom';
-
-
-
-const rootElem = document.querySelector('#root');
-
-const userData = {
-    firstName: 'John',
-    lastName: 'Doe',
-    birthDate: '1991-01-17T11:11:11.819Z',
-    birthPlace: 'London'
-}
-
-ReactDOM.render(<Profile userData={userData}/>, rootElem);
 
 
 
 
-const getDate = date => moment(date).format("DD MMM YY");
+
+const date = date => moment(date).format("DD MMM YY");
 
 
 const Profile = props => {
     return (
-        <>
+        <div>
             <div className="profile__name">
                 {`${props.userData.firstName} ${props.userData.lastName}`}
             </div>
             <div className="profile__birth">
-                {`Was born ${getDate(props.userData.birthDate)} in ${props.userData.birthPlace}`}
+                {`Was born ${date(props.userData.birthDate)} in ${props.userData.birthPlace}`}
             </div>
-        </>
+        </div>
     )
 };
 export default Profile;
