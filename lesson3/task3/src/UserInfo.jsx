4cd24b9690265ef2e2d2f80a96822fe23b483b12
Slava <slavkaklimov@gmail.com>
2020-03-17 16:06:35 +0200

    www

--- lesson3/task3/src/UserInfo.jsx
+++ lesson3/task3/src/UserInfo.jsx
@@ -6,7 +6,7 @@ const UserInfo = props => {
     return (
         <div className="user-info">
             <Avatar avatarUrl={props.author.avatarUrl}
-            name={props.author.name}
+            name={props.user.name}
             />
             <div className="user-info__name">
                 {props.user.name}
