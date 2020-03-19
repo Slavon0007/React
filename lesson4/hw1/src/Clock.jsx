6daf347da0db0ff6d1493cc106e0892fdfb723af
Slava <slavkaklimov@gmail.com>
2020-03-19 10:54:43 +0200

    rrrrr

--- lesson4/hw1/src/Clock.jsx
+++ lesson4/hw1/src/Clock.jsx
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import moment, { utc } from 'moment';
 import './clock.scss';
 
-const getLocaleTime = offset => {   
-    const setHours = new Date().setHours(new Date().getHours() + offset);
-    const formatTime = moment(setHours).format('LTS');
-    return formatTime;
-};
+const getLocaleTime = offset => {
+    const currentTime = new Date();
+    const utcOffset = currentTime.getTimezoneOffset() / 60;
+    return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+}
 
 class Clock extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            localeTime: '',
+            localeTime: '7:00:51 AM',
         }
 
         setInterval(() => {
@@ -25,7 +25,7 @@ class Clock extends Component {
         return (
             <div className="clock">
                 <div className="clock__location">{this.props.location}</div>
-                <div className="clock__time">{this.state.localeTime}</div>
+                <div className="clock__time">{this.state.date.toLocaleTimeString()}</div>
             </div>
         );
     }
