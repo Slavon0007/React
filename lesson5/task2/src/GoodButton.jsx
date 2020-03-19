68af4e08286a1820990fd11ec4b1ad1ccbaccc7c
Slava <slavkaklimov@gmail.com>
2020-03-19 17:15:45 +0200

    wwwwwwwwwwwwwwwwwwwwww

--- lesson5/task2/src/GoodButton.jsx
+++ lesson5/task2/src/GoodButton.jsx
@@ -4,7 +4,7 @@ import './styles.scss'
 
 class GoodButton extends React.Component{
     handleClick(e){
-        alert(document.querySelector('.fancy-button').textContent)
+        alert(e.target.textContent);
     }
 
     render(){
