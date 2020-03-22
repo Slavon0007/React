a03af681ce443c9a176929b11a565f432abdc868
Slava <slavkaklimov@gmail.com>
2020-03-22 11:27:36 +0200

    rrrrr

---
+++ lesson5/hw2/src/ColorPicker.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+const CORAL = 'Coral';
+const AQUA ='Aqua';
+const BISQUE ='Bisque';
+
+
+
+
+class ColorPIcker extends Component{
+
+
+  constructor(props){
+    super(props);
+    this.state = {
+      elem:''
+    };
+  }
+  setColorName = colorName =>{
+    this.setState({
+      elem:colorName
+    });
+  }
+  clearColor=()=>{
+    this.setState({
+      elem:''
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <div className="picker__title">
+          {this.state.elem}
+        </div>
+        <div>
+          <button
+             className="picker__button picker__button_coral"
+             onMouseOver={() => this.setColorName(CORAL)}
+             onMouseOut={() => this.clearColorField()}   
+          >
+         
+          </button>
+          <button
+             className="picker__button picker__button_aqua"
+             onMouseOver={() => this.setColorName(AQUA)}
+             onMouseOut={() => this.clearColorField()}
+          >
+            
+          </button>
+          <button
+             className="picker__button picker__button_bisque"
+             onMouseOver={() => this.setColorName(BISQUE)}
+             onMouseOut={() => this.clearColorField()}   
+          >
+         
+          </button>
+        </div>
+
+      </div>
+
+    )
+  }
+  }
+ 
+export default ColorPIcker;
