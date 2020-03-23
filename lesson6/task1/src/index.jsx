276731adc2549a6a640d0dee20c6eb2e6ab354b1
Slava <slavkaklimov@gmail.com>
2020-03-23 17:55:52 +0200

    eeee

---
+++ lesson6/task1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import Auth from './Auth';
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Auth /> , rootElem);
