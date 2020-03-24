43db1656efdb8b9ccd9a6da0afc1d3fe519c2833
Slava <slavkaklimov@gmail.com>
2020-03-24 07:16:13 +0200

    www

--- lesson6/task3/src/Page.jsx
+++ lesson6/task3/src/Page.jsx
@@ -1,32 +1,30 @@
-import React from 'react';
-import Massage from './Message';
+  
+import React, { Component } from 'react';
+import Message from './Message';
 
-const text1 = "Hello , world";
-const text2 = "Anoher exciting text.";
+const text1 = 'Hello, world!';
+const text2 = 'Another exciting text.'
 
-
-
-class Page extends React.Component{
-    state ={
-        text =null;
-    };
-    setState = text =>{
-        this.setState({
-            text
-        });
-    }
-    render(){
-        return(
-            <div className="page">
-                <Message text={this.state.text} />
-                <div className="actions">
-                    <button className="btn" onClick={() => this.setText(text1)}>Text 1</button>
-                    <button className="btn" onClick={() => this.setText(text2)}>Text 2</button>
-                    <button className="btn" onClick={() => this.setText(null)}>Clear</button>
-                </div>
-            </div>
-        )
-    }
-}
-
+class Page extends Component {
+  state = {
+    text:null
+  };
+  setText = text => {
+    this.setState({
+      text
+    });
+  }
+  render() {
+    return (
+      <div className="page">
+        <Message text={this.state.text} />
+        <div className="actions">
+          <button className="btn" onClick={() => this.setText(text1)}>Text 1</button>
+          <button className="btn" onClick={() => this.setText(text2)}>Text 2</button>
+          <button className="btn" onClick={() => this.setText(null)}>Clear</button>
+        </div>
+      </div>
+    );
+  }
+};
 export default Page;
