b30e14284424887c0161cdba0f4c0b5ec11a555d
Slava <slavkaklimov@gmail.com>
2020-03-24 07:09:44 +0200

    www

---
+++ lesson6/task3/src/Page.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Massage from './Massage';
+
+const text1 = "Hello , world";
+const text2 = "Anoher exciting text.";
+
+
+
+class Page extends React.Component{
+    state ={
+        text =null;
+    };
+    setState = text =>{
+        this.setState({
+            text
+        });
+    }
+    render(){
+        return(
+            <div className="page">
+                <Message text={this.state.text} />
+                <div className="actions">
+                    <button className="btn" onClick={() => this.setText(text1)}>Text 1</button>
+                    <button className="btn" onClick={() => this.setText(text2)}>Text 2</button>
+                    <button className="btn" onClick={() => this.setText(null)}>Clear</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Page;
---
+++ lesson6/task3/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import Massage from './Massage';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Massage />,rootElement);
