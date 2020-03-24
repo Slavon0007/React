305e082896fb3d22ed418de12729e18d01e44048
Slava <slavkaklimov@gmail.com>
2020-03-24 07:31:44 +0200

    qqqq

---
+++ lesson6/hw1/src/Online.jsx
@@ -0,0 +1,18 @@
+  
+import React, { Component } from 'react';
+
+
+
+
+
+class Online extends Component {
+ 
+  render() {
+    return (
+      <>
+       All good!
+      </>
+    );
+  }
+};
+export default Online;
