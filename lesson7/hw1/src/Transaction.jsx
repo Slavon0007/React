01985433bf286093fac2e5a0aaad79b55729023c
Slava <slavkaklimov@gmail.com>
2020-04-12 19:50:46 +0300

    hw1

---
+++ lesson7/hw1/src/Transaction.jsx
@@ -0,0 +1,24 @@
+  
+import React from 'react';
+import './index.scss';
+import moment from 'moment';
+
+
+const Transaction = props => {
+    
+    const amount = new Intl.NumberFormat('en-GB').format(props.amount);
+    const time = moment(props.time).format('HH:mm');
+    const date = moment(props.date).format('DD MMM');
+
+    return (
+        <li className="transaction">
+            <span className="transaction__date">{date}</span>
+            <span className="transaction__time">{time}</span>
+            <span className="transaction__assets">{`${props.from} → ${props.to}`}</span>
+            <span className="transaction__rate">{props.rate}</span>
+            <span className="transaction__amount">{amount}</span>
+        </li>
+    );
+};
+
+export default Transaction;
---
+++ lesson7/hw1/src/TransactiosList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+import Transaction from './Transaction';
+
+const TransactionsList = props => {
+
+    return (
+        <ul className="transactions">
+            {props.transactions.map(transaction => <Transaction key={transaction.id} {...transaction} />)}
+        </ul>
+    )
+}
+export default TransactionsList;
